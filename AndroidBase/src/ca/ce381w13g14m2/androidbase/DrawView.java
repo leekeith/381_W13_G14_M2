4fa6c36facebe265e3c03f45af9fcfa79f75b07d
g19-mr <syed.raza@alumni.ubc.ca>
2013-11-13 10:04:44 -0800

    Android with layout

---
+++ AndroidBase/src/ca/ce381w13g14m2/androidbase/DrawView.java
@@ -0,0 +1,80 @@
+package ca.ce381w13g14m2.androidbase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+
+public class DrawView extends View implements OnTouchListener {
+private static final String TAG = "DrawView";
+
+    List<Point> points = new ArrayList<Point>();
+    Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    Path path = new Path();
+
+    public DrawView(Context context, AttributeSet attribute_set) {
+        super(context, attribute_set );
+        setFocusable(true);
+        setFocusableInTouchMode(true);
+
+        this.setOnTouchListener(this);
+        paint.setStrokeWidth(8);
+        paint.setStyle(Paint.Style.STROKE);
+
+        paint.setColor(Color.RED);
+        paint.setAntiAlias(true);
+    }
+
+    @Override
+/*    public void onDraw(Canvas canvas) {
+        for (Point point : points) {
+            canvas.drawCircle(point.x, point.y, 5, paint);
+            // Log.d(TAG, "Painting: "+point);
+        }
+    }*/
+    
+    public void onDraw(Canvas canvas) {
+        boolean first = true;
+        for(Point point : points){
+            if(first){
+                first = false;
+                path.moveTo(point.x, point.y);
+            }
+            else{
+                path.lineTo(point.x, point.y);
+            }
+        }
+        canvas.drawPath(path, paint);
+        
+    }
+
+    public boolean onTouch(View view, MotionEvent event) {
+        // if(event.getAction() != MotionEvent.ACTION_DOWN)
+        // return super.onTouchEvent(event);
+        Point point = new Point();
+        point.x = event.getX();
+        point.y = event.getY();
+        points.add(point);
+        invalidate();
+        Log.d(TAG, "point: " + point);
+        return true;
+    }
+}
+
+class Point {
+    float x, y;
+
+    @Override
+    public String toString() {
+        return x + ", " + y;
+    }
+}
--- AndroidBase/src/ca/ce381w13g14m2/androidbase/MainActivity.java
+++ AndroidBase/src/ca/ce381w13g14m2/androidbase/MainActivity.java
@@ -1,15 +1,18 @@
 package ca.ce381w13g14m2.androidbase;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
-
+	DrawView drawView;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_main); 
+
 	}
 
 	@Override
@@ -18,5 +21,13 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-
+/*	
+	// Called when the user clicks the Send button 
+	public void onButtonClick(View view) {
+	    //Intent intent = new Intent(this, GFXSurface.class);
+	    //startActivity(intent);
+		Intent intent = new Intent(this, Draw.class);
+	    startActivity(intent);
+	}
+*/	
 }
