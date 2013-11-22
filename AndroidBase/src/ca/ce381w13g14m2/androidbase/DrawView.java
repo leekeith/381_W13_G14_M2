3f77fa6f7e787436e048d29a3d223eca4d18a2cf
Jonathan Machel <j_machel@hotmail.com>
2013-11-22 07:58:38 -0800

    Updated clear button
    Added a timer to check the clear button rather than waiting for a click
    to clear the screen.

--- AndroidBase/src/ca/ce381w13g14m2/androidbase/DrawView.java
+++ AndroidBase/src/ca/ce381w13g14m2/androidbase/DrawView.java
@@ -2,6 +2,8 @@ package ca.ce381w13g14m2.androidbase;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import android.content.Context;
 import android.graphics.Canvas;
@@ -25,23 +27,23 @@ public static int i = 0;
     Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
     Paint paint_border = new Paint(Paint.ANTI_ALIAS_FLAG);
     Path path = new Path();
+    //List<Path> paths = new ArrayList<Path>();
 
     public DrawView(Context context, AttributeSet attribute_set) {
         super(context, attribute_set );
         setFocusable(true);
         setFocusableInTouchMode(true);
-
         this.setOnTouchListener(this);
-        paint.setStrokeWidth(8);
+        
         paint.setStyle(Paint.Style.STROKE);
-        paint.setColor(color);
-        paint_border.setStrokeWidth(2);
         paint_border.setStyle(Paint.Style.STROKE);
 
-        paint.setColor(Color.RED);
         paint.setAntiAlias(true);
-        paint_border.setColor(Color.BLUE);
         paint_border.setAntiAlias(true);
+        
+        Timer my_timer = new Timer(); 
+        MyTimerTask my_task = new MyTimerTask(); 
+        my_timer.schedule(my_task, 1000, 100);
         
     }
 
@@ -71,23 +73,93 @@ public static int i = 0;
     }
 
     public boolean onTouch(View view, MotionEvent event) {
-        // if(event.getAction() != MotionEvent.ACTION_DOWN)
-        // return super.onTouchEvent(event);
-        Point point = new Point();
+        
+        
+    	Point point = new Point();
         point.x = event.getX();
         point.y = event.getY();
         points.add(point);
         invalidate();
-        //Log.d(TAG, "point: " + point);
+        Log.d(TAG, "point: " + point);
         
-        i = ((int)point.y * 600) + (int)point.x;
+        i = ((int)point.y * 320/600) + (int)point.x;
         send_data = Integer.toString(i);
         Log.d(TAG, send_data);
         
         if (event.getAction() == android.view.MotionEvent.ACTION_UP)
         	points.clear();
+    	
+    	
+    	
+    	/*
+    	final MotionEvent fevent = event;
+        new Thread(new Runnable() {
+            public void run() {
+            	Point point = new Point();
+                point.x = fevent.getX();
+                point.y = fevent.getY();
+                points.add(point);
+                invalidate();
+                Log.d(TAG, "point: " + point);
+                
+                i = ((int)point.y * 600) + (int)point.x;
+                send_data = Integer.toString(i);
+                Log.d(TAG, send_data);
+                
+                if (fevent.getAction() == android.view.MotionEvent.ACTION_UP)
+                	points.clear();
+            }
+        }).start();
+    	*/
+    	
+    	
+        //new SavePointTask().execute(event);
         return true;
     }
+
+
+
+   
+    
+/*
+    private class SavePointTask extends AsyncTask<MotionEvent, Void, Point> {
+        //* The system calls this to perform work in a worker thread and
+          //* delivers it the parameters given to AsyncTask.execute() 
+        protected Point doInBackground(MotionEvent... event) {
+        	Point point = new Point();
+            point.x = event[0].getX();
+            point.y = event[0].getY();
+            points.add(point);
+        	postInvalidate();
+
+            if (event[0].getAction() == android.view.MotionEvent.ACTION_UP)
+            	points.clear();
+            return point;
+        }
+        
+        //* The system calls this to perform work in the UI thread and delivers
+          // the result from doInBackground() 
+        protected void onPostExecute(Point point) {
+        	//invalidate();
+
+        	i = ((int)point.y * 600) + (int)point.x;
+            send_data = Integer.toString(i);
+            Log.d(TAG, send_data);
+        }
+    }*/
+    
+    
+    
+    public class MyTimerTask extends TimerTask { 
+    	 public void run() { 
+    		 if(clear == true)
+    		 {
+    			 postInvalidate();
+    		 }
+    	 } 
+    } 
+
+ 
 }
 
 class Point {
